9a547fb5519e0972a652756ad83b68fdcaa97ef3
Jamie Han <jamie@snowkiwi.com>
2018-12-16 16:06:40 +0900

    commit

--- CH17.정규표현식/17.8문자셋.js
+++ CH17.정규표현식/17.8문자셋.js
@@ -8,9 +8,9 @@ const matches = beer99.match(/0|1|2|3|4|5|6|7|8|9/g);   // [ '9', '9', '1', '9',
 const m1 = beer99.match(/[0123456789]/g);       // [ '9', '9', '1', '9', '8' ]
 const m2 = beer99.match(/[0-9]/g);              // [ '9', '9', '1', '9', '8' ]
 
-// const match = beer99.match(/[\-0-9a0z.]/ig);    // [ '9', '9', 'a', 'a', '1', 'a', 'a', 'a', '-', '-', '9', '8', 'a', '.' ]
+const match = beer99.match(/[\-0-9a-z.]/ig);    //[ '9', '9', 'b', 'o', 't', 't', 'l', 'e' ... '.' ]
 
-const match = beer99.match(/[^\-0-9a-z.]/);
+// const match = beer99.match(/[^\-0-9a-z.]/);
 // [ ' ',
 //   index: 2,
 //   input: '99 bottles of beer on the wall take 1 down and pass it around -- 98 bottles of beer on the wall.',
--- CH20.노드/20.10웹서버.js
+++ CH20.노드/20.10웹서버.js
@@ -1,25 +1,25 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer(function(req, res) {
-    console.log(`${req.method} ${req.url}`);
-    res.end('Hello world!');
-});
+// const server = http.createServer(function(req, res) {
+//     console.log(`${req.method} ${req.url}`);
+//     res.end('Hello world!');
+// });
 
-const port = 8080;
-server.listen(port, function() { 
-    // 서버가 시작됐을 때 호출될 콜백을 넘길 수 있다.
-    console.log(`server started on port ${port}`);
-});
+// const port = 8080;
+// server.listen(port, function() { 
+//     // 서버가 시작됐을 때 호출될 콜백을 넘길 수 있다.
+//     console.log(`server started on port ${port}`);
+// });
 
 
 
-const server = http.createServer(function(req, res) {
-    if (req.method === 'GET' && req.url === '/favicon.ico') {
-        const fs = require('fs');
-        fs.createReadStream('favicon.ico');
-        fs.pipe(res);
-    } else {
-        console.log(`${req.method} ${req.url}`);
-        res.end('Hello world!')
-    }
-});
+// const server = http.createServer(function(req, res) {
+//     if (req.method === 'GET' && req.url === '/favicon.ico') {
+//         const fs = require('fs');
+//         fs.createReadStream('favicon.ico');
+//         fs.pipe(res);
+//     } else {
+//         console.log(`${req.method} ${req.url}`);
+//         res.end('Hello world!')
+//     }
+// });

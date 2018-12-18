579c0560a860b195b29e7637563a6aba7bf26e02
Jamie Han <jamie@snowkiwi.com>
2018-12-18 16:51:33 +0900

    commit

--- CH17.정규표현식/17.1부분_문자열_검색과_대체.js
+++ CH17.정규표현식/17.1부분_문자열_검색과_대체.js
@@ -1,10 +1,10 @@
 const input = "As I was going to Saint Ives";
 // input.startsWith("As");              // true
 // input.endsWith("Ives");              // true
-// input.startsWith("going", 9);        // true
-// input.endsWith("going", 14);         // true
+// input.startsWith("going", 9);        // true     -- 인덱스 9에서 시작
+// input.endsWith("going", 14);         // true     -- 인덱스 14를 문자열 끝으로 간주
 // input.includes("going");             // true
-// input.includes("going", 10);         // false
+// input.includes("going", 10);         // false    -- 인덱스 10에서 시작하면 "going"은 없다.
 // input.indexOf("going");              // 9
 // input.indexOf("going", 10);          // -1
 // input.indexOf("nope");               // -1
